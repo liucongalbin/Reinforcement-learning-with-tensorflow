1fda359fe136494c29c717c1b396a6216d8e798e
zhangliying-123 <1416860164@qq.com>
2019-11-25 15:05:50 +0800

    添加AC_CartPole代码的中文注释

--- contents/Actor_Critic_Advantage_8/AC_CartPole.py
+++ contents/Actor_Critic_Advantage_8/AC_CartPole.py
@@ -31,7 +31,9 @@ env = gym.make('CartPole-v0')
 env.seed(1)  # reproducible
 env = env.unwrapped
 
+# 状态的维度（车的坐标、杆的角度、杆的角速度、杆的角加速度？）
 N_F = env.observation_space.shape[0]
+# 动作的维度（左/右施加力）
 N_A = env.action_space.n
 
 
@@ -44,11 +46,16 @@ class Actor(object):
         self.td_error = tf.placeholder(tf.float32, None, "td_error")  # TD_error
 
         with tf.variable_scope('Actor'):
+
+            # 两层全连接网络
             l1 = tf.layers.dense(
                 inputs=self.s,
+                # 对于l1来说，属于输出。对于Actor来说，属于隐/中间状态
                 units=20,    # number of hidden units
                 activation=tf.nn.relu,
+                # 权重矩阵的初始化函数
                 kernel_initializer=tf.random_normal_initializer(0., .1),    # weights
+                # bias的初始化函数
                 bias_initializer=tf.constant_initializer(0.1),  # biases
                 name='l1'
             )
@@ -62,22 +69,30 @@ class Actor(object):
                 name='acts_prob'
             )
 
+        # 损失函数
         with tf.variable_scope('exp_v'):
             log_prob = tf.log(self.acts_prob[0, self.a])
             self.exp_v = tf.reduce_mean(log_prob * self.td_error)  # advantage (TD_error) guided loss
 
+        # 优化器
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)  # minimize(-exp_v) = maximize(exp_v)
 
+    # 训练
+    # 传入：状态、动作、TD误差
     def learn(self, s, a, td):
         s = s[np.newaxis, :]
         feed_dict = {self.s: s, self.a: a, self.td_error: td}
         _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
         return exp_v
 
+    # 预测：动作
+    # 基于：状态
     def choose_action(self, s):
         s = s[np.newaxis, :]
+        # 模型结果
         probs = self.sess.run(self.acts_prob, {self.s: s})   # get probabilities for all actions
+        # 依照概率选择动作
         return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())   # return a int
 
 
@@ -86,7 +101,9 @@ class Critic(object):
         self.sess = sess
 
         self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+        # 预测/评估的（状态t+1的）价值
         self.v_ = tf.placeholder(tf.float32, [1, 1], "v_next")
+        # 实际的reward
         self.r = tf.placeholder(tf.float32, None, 'r')
 
         with tf.variable_scope('Critic'):
@@ -101,6 +118,7 @@ class Critic(object):
                 name='l1'
             )
 
+            # 预测/评估的（状态t的）价值
             self.v = tf.layers.dense(
                 inputs=l1,
                 units=1,  # output units
@@ -111,15 +129,21 @@ class Critic(object):
             )
 
         with tf.variable_scope('squared_TD_error'):
+            # TD误差公式
             self.td_error = self.r + GAMMA * self.v_ - self.v
+            # 平方
             self.loss = tf.square(self.td_error)    # TD_error = (r+gamma*V_next) - V_eval
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
 
+    # 传入：状态、奖励、下一个状态
     def learn(self, s, r, s_):
         s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
 
+        # 预测价值函数
         v_ = self.sess.run(self.v, {self.s: s_})
+
+        # 训练，减少且返回TD误差
         td_error, _ = self.sess.run([self.td_error, self.train_op],
                                           {self.s: s, self.v_: v_, self.r: r})
         return td_error
@@ -127,6 +151,7 @@ class Critic(object):
 
 sess = tf.Session()
 
+# 网络初始化
 actor = Actor(sess, n_features=N_F, n_actions=N_A, lr=LR_A)
 critic = Critic(sess, n_features=N_F, lr=LR_C)     # we need a good teacher, so the teacher should learn faster than the actor
 
@@ -136,23 +161,33 @@ if OUTPUT_GRAPH:
     tf.summary.FileWriter("logs/", sess.graph)
 
 for i_episode in range(MAX_EPISODE):
+
+    # 得到状态s
     s = env.reset()
+    # 时间步为：0
     t = 0
+    # 记录路径轨迹上的reward列表
     track_r = []
     while True:
         if RENDER: env.render()
 
+        # actor根据状态s选择动作a
         a = actor.choose_action(s)
 
+        # 环境根据动作a到达下一个状态s_，得到reward：r，即是否完成：done
         s_, r, done, info = env.step(a)
 
         if done: r = -20
 
+        # 记录reward。后续计算episode总reward用
         track_r.append(r)
 
+        # 训练critic网络，并得到TD误差
         td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
+        # 使用TD误差，加上状态s，动作a，训练actor网络
         actor.learn(s, a, td_error)     # true_gradient = grad[logPi(s,a) * td_error]
 
+        # 状态、时间步index更新表示
         s = s_
         t += 1
 
